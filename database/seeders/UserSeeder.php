f96e493910bc3a55368ccb2b98c6c2b30f377191
Md. Ashraful Islam <mdashrafulislam369@gmail.com>
2025-05-17 15:37:25 +0600

    user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['id' => 1],
+            [
+                'username' => 'admin',
+                'name' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('admin123'),
+                'status' => 3,
+            ]
+        );
+    }
+}
