a91182ca9e14dfdb8fa9fadfe46918a90e19d723
tariqul-babylon <tariqul@newgen-bd.com>
2025-05-18 16:48:49 +0600

    update

--- routes/frontend.php
+++ routes/frontend.php
@@ -7,4 +7,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('join-exam', [JoinExamController::class, 'joinExam'])->name('front.join-exam');
 Route::post('join-exam', [JoinExamController::class, 'joinExamSubmit'])->name('front.join-exam-submit');
 
+Route::get('exam/{exam_code}', [JoinExamController::class, 'exam'])->name('front.exam');
+Route::post('exam/{exam_code}', [JoinExamController::class, 'examSubmit'])->name('front.exam-submit');
+Route::get('exam/{exam_code}/alert', [JoinExamController::class, 'examAlert'])->name('front.exam-alert');
+
 Route::get('exam-form/{exam_code}', [JoinExamController::class, 'examForm'])->name('front.exam-form');
